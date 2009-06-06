68c757fe0db56ebe5c10fa8473ddfdda2ad51e3c
Kaleb Walton <kalebwalton@gmail.com>
2009-06-05 22:12:11 -0400

    Fixed internal files. Fixed photo resize problem.

--- app/controllers/admin/managed_files_controller.rb
+++ app/controllers/admin/managed_files_controller.rb
@@ -1,5 +1,14 @@
 class Admin::ManagedFilesController < Admin::ResourceController
 
+  def create
+    uploaded_data = params[:managed_file][:uploaded_data] unless params[:managed_file].nil?
+    # Make sure we have a content-type if it came in as nil
+    uploaded_data.content_type = "text/plain" if !uploaded_data.nil? && uploaded_data.content_type.nil?
+    model.update_attributes!(params[model_symbol])
+    announce_saved
+    response_for :create
+  end
+
   def index
     conditions = case params[:file_type]
       when 'photos'     then 'content_type LIKE "image%" AND thumbnail IS NULL'
@@ -9,7 +18,11 @@ class Admin::ManagedFilesController < Admin::ResourceController
       when 'other'      then 'content_type NOT LIKE "image%" AND content_type NOT LIKE "audio%" AND content_type NOT LIKE "video%" AND content_type NOT LIKE "application/x-shockwave-flash"'
       else                   'thumbnail IS NULL'
     end
-    
+
+    unless current_user.admin? || current_user.developer? || (current_user.respond_to?(:site_admin?) ? current_user.site_admin? : false)
+      conditions += ' AND internal = 0'
+    end
+
     @managed_files = ManagedFile.paginate :page => params[:page], :order => 'filename', :conditions => conditions
 
     # Support for fckeditor file selector
--- app/models/managed_file.rb
+++ app/models/managed_file.rb
@@ -1,6 +1,11 @@
 class ManagedFile < ActiveRecord::Base
-  has_attachment :storage => :file_system, :thumbnails => { :thumb => '140>' },
+  has_attachment :storage => :file_system, :thumbnails => { :thumb => '75>' },
                      :resize_to => "300>", :max_size => 25.megabytes
   validates_as_attachment
-  validates_uniqueness_of :filename
+  validates_uniqueness_of :filename, :message => "is already in use"
+
+  # Fixes the image file size after being resized
+  after_resize do |record, img|
+    record.size = File.size(record.temp_path)
+  end
 end
