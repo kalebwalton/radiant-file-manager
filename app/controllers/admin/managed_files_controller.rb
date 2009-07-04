dfe12f908e19709fb9134b41882f6758f41a39d4
Kaleb Walton <kalebwalton@gmail.com>
2009-07-04 10:58:40 -0400

    fixed file browser

--- app/controllers/admin/managed_files_controller.rb
+++ app/controllers/admin/managed_files_controller.rb
@@ -14,7 +14,7 @@ class Admin::ManagedFilesController < Admin::ResourceController
       when 'photos'     then 'content_type LIKE "image%" AND thumbnail IS NULL'
       when 'flash'      then 'content_type LIKE "application/x-shockwave-flash"'
       when 'multimedia' then 'content_type LIKE "audio%" OR content_type LIKE "video%"'
-      when 'internal'   then 'internal = true'
+      when 'internal'   then 'internal = 1'
       when 'other'      then 'content_type NOT LIKE "image%" AND content_type NOT LIKE "audio%" AND content_type NOT LIKE "video%" AND content_type NOT LIKE "application/x-shockwave-flash"'
       else                   'thumbnail IS NULL'
     end
